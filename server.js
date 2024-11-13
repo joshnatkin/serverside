5a0ad3263d3805b3d68cd3701ba77221085da898
joshnatkin <jnatkin@email.sc.edu>
2024-11-13 16:29:41 -0500

    serverside adding dogs

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.post("/api/dogs", upload.single("dogImage"), (req, res) => {
   };
 
   Dogs.animals.push(newDog);
-  res.status(200).json(newDog);
+  res.status(200).json(Dogs.animals);  // Sends full list of dogs after adding a new one
 });
 
 // File upload route for testing image upload separately (optional)
